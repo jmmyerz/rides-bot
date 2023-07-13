134028df40e9b1e1bee379d501cfbd9d861e2ff7
Jordan Myers <jmmyerz@gmail.com>
2023-07-13 12:52:55 -0600

    Change wsgi entry point

--- rides_bot/callback_server.py
+++ rides_bot/callback_server.py
@@ -1,7 +1,9 @@
 from flask import Flask, Response, request
 
-from .rides_bot import run_bot
+from utils.config import Config
+from .rides_bot import run_bot, CONFIG_FILE_PATH
 
+config = Config().load(CONFIG_FILE_PATH)
 
 class UpdateArgs(object):
 
@@ -17,21 +19,9 @@ app = Flask(__name__)
 @app.post('/update')
 def groupme():
     data = request.get_json()
-    # TODO: Push these args into the config file
-    update_args = {
-        'debug': False,
-        'groupme': True,
-        'date': False,
-        'gm_debug': False,
-        'login': False,
-        'message': False,
-    }
+
     if data.__getitem__('text').lower().strip() == 'refresh':
-        run_bot(UpdateArgs(update_args))
+        run_bot(UpdateArgs(config.gunicorn.rides_bot_args))
         return Response(status=200)
     else:
         return Response(status=204)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7045, debug=True)
--- rides_bot/wsgi.py
+++
@@ -1,4 +0,0 @@
-from .callback import app
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7045, debug=False)
