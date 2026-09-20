9956a8254d6bbd03d24a1d0ee7fc70c9ff0b9585
Jordan Myers <jmmyerz@gmail.com>
2026-09-20 12:13:43 -0600

    test entrypoint behavior

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,6 +11,7 @@
 #   listener  - runs the bot listener (discord, groupme, telegram)
 #   debug     - runs the container in debug mode (sleeps indefinitely so you can attach and debug)
 #
+# ### This note is a test!
 set -e
 
 # Sync the /rides-bot repo
